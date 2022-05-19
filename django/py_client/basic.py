901c658a8b7d64d0ae833d49e9c378e3b1ee1471
Abadbeginning <2608403303@qq.com>
2022-05-19 00:49:02 +0000

    Create your first API View

--- django/py_client/basic.py
+++ django/py_client/basic.py
@@ -2,6 +2,7 @@ import requests
 
 entpoint = "https://httpbin.org/status/200/"
 endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint)
 print("text ==>", get_response.text)
