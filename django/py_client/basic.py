bf88eb0505a3ff7d10e2144266c381440de51e8b
Abadbeginning <2608403303@qq.com>
2022-05-19 00:24:55 +0000

    首个django项目

--- django/py_client/basic.py
+++ django/py_client/basic.py
@@ -1,1 +1,11 @@
 import requests
+
+entpoint = "https://httpbin.org/status/200/"
+endpoint = "https://httpbin.org/anything"
+
+get_response = requests.get(endpoint)
+print("text ==>", get_response.text)
+
+# HTTP Request -> HTML
+# REST API HTTP Request
+print("json ==>", get_response.json())
